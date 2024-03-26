7fed2a1ce66b6f7f4a045cc6bdc22c34a4d4f98f
Alex Berkhout <alexander.berkhout@marquetteenergyanalytics.com>
2024-03-25 21:16:15 -0600

    add ability to select random context

--- src/context_npc/__init__.py
+++ src/context_npc/__init__.py
@@ -1,18 +1,65 @@
+from dataclasses import dataclass
+import json
 import os
+import random
+from typing import Optional
 from openai import OpenAI
 import requests
 import re
 
+@dataclass
+class ApiInfo:
+  count: int
+  pages: int
+  next: Optional[str]
+  previous: Optional[str]
+
+@dataclass
 class ArticleType:
   LOCATIONS = "locations"
   FAMILIES = "families"
   CHARACTERS = "characters"
-  ORGANISATIONS = "organizations"
+  ORGANISATIONS = "organisations"
   ITEMS = "items"
   NOTES = "notes"
   CALENDARS = "calendars"
   RACES = "races"
   QUESTS = "quests"
+  
+  
+class Character:
+
+
+  def __init__(self, name, entry,age, sex, appearance, personality):
+    self.name = name
+    self.entry = entry
+    self.age = age
+    self.sex = sex 
+    self.appearance = appearance
+    self.personality = personality
+    
+  def __str__(self):
+    return f"{self.name} : {self.entry}"
+  
+  def submit(self, campaign_id: str):
+    url = "https://api.kanka.io/1.0/campaigns/" + campaign_id + "/characters"
+    data = {
+      "name": self.name,
+      "entry": self.entry,
+      "age": self.age,
+      "sex": self.sex,
+      "appearance_name": ["Appearance"],
+      "appearance_entry": [self.appearance],
+      "personality_name": ["Personality"],
+      "personality_entry": [self.personality]
+    }
+    
+    headers = {
+      "Authorization": "Bearer " + os.getenv("KANKA_API"),
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+      
     
 
 class NPCGenerator:
@@ -25,11 +72,10 @@ class NPCGenerator:
     , "content": "You are used to generate new NPCS for a campaign given context from a user, which can be a location, family, character, organization, item."
     },
     { "role": "system"
-    , "content": "Return the response as a json with the fields name, entry (an html description of the characters history and including a section on goals, fears and bonds), title, age, sex and location"
+    , "content": "Return the response as a json with the fields name, entry (an html description of the characters history and including a section on goals, fears and bonds), age, sex, races (as an array) ."
     },
-    {
-      "role": "system",
-      "content": "The relatedness parameter is an integer from 1 to 10, where 1 is the least related and 10 is the most related. It is used to determine how closely the generated NPC should be related to the context given by the user."
+    { "role": "system"
+    , "content": "Additionally provide in the json an appearance field which is a description of the characters physical appearance and a personality field which is a description of the characters personality."
     }
   ]
   
@@ -47,9 +93,8 @@ class NPCGenerator:
     )
     
     
-  def gen_character(self, content, relatedness: int = 5):
+  def gen_character(self, content):
     context = [ 
-        { "role": "user", "content": "Relatedness: " + str(relatedness)},
         { "role": "user", "content": content }
       ]
     response = self.client.chat.completions.create(
@@ -57,12 +102,57 @@ class NPCGenerator:
         messages=self.instructions + context,
         temperature=0
     )
-    response_message = response.choices[0].message.content
-    return response_message
+    response_message = json.loads(response.choices[0].message.content)
+    return Character(
+      name = response_message["name"],
+      entry = response_message["entry"],
+      age = response_message["age"],
+      sex = response_message["sex"],
+      personality= response_message["personality"],
+      appearance = response_message["appearance"]
+    )
   
-  def _location_context(self, location_name: str):
-    url = self.root_url + self.campaign_id + "/locations?name=" + location_name
+  def _generic_context(self, context_type: str, name: str):
+    url = self.root_url + self.campaign_id + "/" + context_type + "?name=" + name
     response = requests.get(url, headers=self.headers)
-    location_contents = response.json()
-    stripped_html = re.sub('<[^<]+?>', '', location_contents["data"][0]["entry_parsed"])
+    contents = response.json()
+    stripped_html = re.sub('<[^<]+?>', '', contents["data"][0]["entry_parsed"])
+    return stripped_html
+  
+  
+  def _get_article_page(self, url):
+    response = requests.get(url, headers=self.headers)
+    contents = response.json()
+    info = ApiInfo(
+      count = contents["meta"]["total"],
+      pages = contents["meta"]["last_page"],
+      next = contents["links"]["next"],
+      previous = contents["links"]["prev"])
+    return contents["data"], info
+  
+  def _get_article_list(self, context_type: str):
+    print(context_type)
+    initial_url = self.root_url + self.campaign_id + "/" + context_type
+    data, info = self._get_article_page(initial_url)
+    while info.next:
+      next_data, info = self._get_article_page(info.next)
+      data += next_data
+    return data
+  
+  def _random_context(self):
+    article_types =  \
+        [ ArticleType.LOCATIONS
+        # , ArticleType.FAMILIES
+        , ArticleType.CHARACTERS
+        , ArticleType.ORGANISATIONS
+        # , ArticleType.ITEMS
+        , ArticleType.NOTES
+        # , ArticleType.CALENDARS
+        , ArticleType.RACES
+        # , ArticleType.QUESTS
+        ]
+    context_type = random.choice(article_types)
+    response = self._get_article_list(context_type)
+    random_article = random.choice(list(filter(lambda r: r["entry_parsed"] is not None, response)))
+    stripped_html = re.sub('<[^<]+?>', '', random_article["entry_parsed"])
     return stripped_html
--- src/context_npc/test.py
+++ src/context_npc/test.py
@@ -1,14 +1,11 @@
 import os
 from dotenv import load_dotenv
-from src.context_npc import NPCGenerator
+from src.context_npc import NPCGenerator, ArticleType
 
 if __name__ == "__main__":
     load_dotenv()
     gen = NPCGenerator(campaign_id = os.getenv("CAMPAIGN_ID"))
-    context = gen._location_context("Cool City")
+    context = gen._random_context()
     npc1 = gen.gen_character(context)
     print(npc1)
-    npc2 = gen.gen_character(context, 1)
-    print(npc2)
-    npc3 = gen.gen_character(context, 10)
-    print(npc3)
+    npc1.submit(gen.campaign_id)
